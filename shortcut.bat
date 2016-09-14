6d76b0b685bd589df76afac97e2fae28b3f6969d
Joel-U <j.uyesugi@wsu.edu>
2016-09-14 15:00:59 -0700

    Updated shortcut.bat

--- shortcut.bat
+++ shortcut.bat
@@ -6,11 +6,11 @@ cls
 ::
 :: HOW TO USE:
 :: To use this .bat file for running and updating Sparkle-
-:: Average-Analysi, make a copy of this file and rename it
+:: AverageAnalysi, make a copy of this file and rename it
 :: something easy to remember (e.g. SparkleAverageAnalysis.bat).
 :: Now in the copy of this file you will want to change the
 :: variable of "location" (line 24) with the path to the
-:: Sparkle-Average-Analysi directory (where you found this file).
+:: SparkleAverageAnalysis directory (where you found this file).
 :: After you complete that you will want to change the
 :: variable for "gitLocation" (line 28) with the path to
 :: where your git.exe is stored. Once you have those set,
@@ -20,8 +20,8 @@ cls
 :: and will ask the user if they wish to update if there
 :: are any new updates.
 ::
-:: Place your path to Sparkle-Average-Analysis here
-set location="C:\Users\Name\Documents\Sparkle-Average-Analysis\"
+:: Place your path to SparkleAverageAnalysis here
+set location="C:\Users\Name\Documents\SparkleAverageAnalysis\"
 cd %location%
 ::
 :: Place your path to Git here
@@ -30,9 +30,9 @@ SET PATH=%PATH%;%gitLocation%
 ::
 :: --------------------------------------------------------
 
-title Sparkle-Average-Analysis
+title SparkleAverageAnalysis
 
-:: Get versions of Sparkle-Average-Analysis
+:: Get versions of SparkleAverageAnalysis
 git remote update
 for /f "delims=" %%i in ('git rev-parse @{0}') do set local=%%i
 for /f "delims=" %%i in ('git rev-parse origin/master') do set remote=%%i
@@ -42,12 +42,12 @@ echo.
 
 :: Check relation of various versions
 if "%local%" equ "%remote%" (
-    echo Sparkle-Average-Analysis is up to date.
+    echo Sparkle Average Analysis is up to date.
     goto :runSparkleAverageAnalysis
 ) else if "%local%" equ "%base%" (
-    echo A newer version of Sparkle-Average-Analysis is avaliable.
+    echo A newer version of Sparkle Average Analysis is avaliable.
 ) else if "%remote%" equ "%base%" (
-    echo Your local branch of Sparkle-Average-Analysis is ahead of origin/master.
+    echo Your local branch of Sparkle Average Analysis is ahead of origin/master.
     goto :runSparkleAverageAnalysis
 )
 
@@ -55,7 +55,7 @@ set "answer=%globalparam1%"
 goto :answerCheck
 
 :updatePrompt
-set /p "answer=Update Sparkle-Average-Analysis? (y or n): "
+set /p "answer=Update SparkleAverageAnalysis? (y or n): "
 goto :answerCheck
 
 :answerCheck
